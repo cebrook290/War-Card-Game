02e05cad33aba1c42a0bf10fbf0a20f25d04bdce
Chris Esterbrook <116049806+cebrook290@users.noreply.github.com>
2023-01-18 10:38:18 -0800

    attempted update

--- WarCardGame.js
+++ WarCardGame.js
@@ -1,5 +1,11 @@
 const suits = ['Spades', 'Diamonds', 'Clubs', 'Hearts'];
 const values = ['2', '3', '4', '5','6','7','8','9','10','J', 'Q', 'K', 'A'];
+const playerHand = [];
+const compHand = [];
+const playDeck = '';
+const computerDeck = '';
+const playerCard = '';
+const compCard = '';
 
 function createDeck() {
     let deck = new Array();
@@ -28,8 +34,64 @@ function shuffle(deck) {
     }
 }
 
+function splitCards(deck) {
+    var i = 0;
+    while (i != deck.length) {
+        playerHand.push(deck[i]);
+        compHand.push(deck[(i+1)]);
+        i += 2;
+    }
+}
+       
+function deal() {
+    playerCard = playerHand[0];
+    compCard = compHand[0];
+    compare(playerCard, compCard);
+}
+
+function compare (player, comp) {
+    
+    if ((player % 13) > (comp % 13)) {
+        
+       playerHand.push(comp);
+       playerHand.push(player);
+       
+       playerHand.shift();
+       compHand.shift();
+       
+       setTimeout(function() {
+           moveCards('player');
+       }, 1500);
+        
+        updateCount();
+        checkWin();
+    } else if ((player % 13) < (comp % 13)) {
+        
+        compHand.push(player);
+		compHand.push(comp);
+        
+        compHand.shift();
+		playerHand.shift();
+        
+        setTimeout(function() {
+			moveCards('comp');
+		}, 1500);
+
+        updateCount();
+		checkWin();
+    } else if ((player % 13) === (comp % 13)) 
+        tie();
+}
+
+function checkWin() {
+    if (playerHand.length == 0) {
+        alert('The Computer wins the game');
+    } else if (compHand.length == 0) {
+        alert('The Player wins the game');
+    }
+}
 
 var deck1 = createDeck();
 shuffle(deck1);
 
-console.log(deck1);
+
