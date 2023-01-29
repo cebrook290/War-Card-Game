4719ba9f02b2b43965e23a90a92449442ddab292
Chris Esterbrook <cebrook290@gmail.com>
2023-01-28 23:02:17 -0800

    ..

--- WarCardGame-test.js
+++ WarCardGame-test.js
@@ -2,12 +2,9 @@ var expect = chai.expect
 
 describe('MyFunctions', function (){
     describe('#declareWinner', function() {
-        it('should declare player one the winner', function() {
+        it('should declare a winner', function() {
             let w = declareWinner();
-            expect(w).to.equal(playerOneName.toUpperCase() + ' WINS!!');
-        });
-        it('should declare player two the winner', function() {
-            expect(declareWinner()).to.equal(playerTwoName.toUpperCase() + ' WINS!!');
+            expect(w).to.equal(winner.toUpperCase() + ' WINS!!');
         });
     });
 });
